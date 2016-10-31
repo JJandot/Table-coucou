cae6c7b6975072db6faa51dabe82bd1cc1693f21
Julien <julienjandot@orange.fr>
2016-10-31 13:37:20 +0100

    Node documentation

--- src/Prefix/Node.java
+++ src/Prefix/Node.java
@@ -1,29 +1,61 @@
 package Prefix;
 
-import java.util.HashMap;
+import Cuckoo.CuckooTable;
+import Cuckoo.HashableString;
 
 public class Node {
 
     private boolean isLeaf;
-    private HashMap<Character, Node> children = new HashMap<>();
-
+    private CuckooTable<HashableString, Node> children = new CuckooTable<>(1);
     private char letter;
 
+    /**
+     * Constructor.
+     * Used for creating the first Node
+     */
     public Node(){}
 
+
+    /**
+     * Constructor.
+     * Used to add new Nodes to the tree
+     * @param letter
+     */
     public Node(char letter) {
         this.letter = letter;
     }
 
-    public HashMap<Character, Node> getChildren() {
+
+    /**
+     * Getter.
+     * @return
+     */
+    public CuckooTable<HashableString, Node> getChildren() {
         return children;
     }
 
+    /**
+     * Setter
+     * @return true if the current node is a leaf
+     */
     public boolean isLeaf() {
         return isLeaf;
     }
 
+
+    /**
+     * Setter.
+     * @param leaf true or false
+     */
     public void setLeaf(boolean leaf) {
         isLeaf = leaf;
+    }
+
+    /**
+     * Getter.
+     * @return the value of the node
+     */
+    public char getLetter(){
+        return letter;
     }
 }
